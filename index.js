3808c7806ea9c9342f25278a341e2f21e93d78b7
Fausto Ramirez <ramirezmedia@gmail.com>
2022-12-12 23:13:18 -0500

    commit tree e1c30536a30e88addd5885e9a9fb0481d093876e

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Your code here
+const dodger = document.getElementById('dodger');
+
+function moveDodgerLeft(){
+    const leftNumber = dodger.style.left.replace('px', '');
+    const left = parseInt(leftNumber, 10);
+
+    if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+    }
+}
+
+function moveDodgerRight(){
+    const rightNumber = dodger.style.left.replace('px', '');
+    const right = parseInt(rightNumber, 10);
+    console.log(right);
+
+    if (right < (400 - 40)) {
+    dodger.style.left = `${right + 1}px`;
+    }
+}
+
+document.addEventListener('keydown', function(e){
+    if(e.key === 'ArrowRight'){
+        moveDodgerRight();
+    }
+
+    if(e.key === 'ArrowLeft'){
+        moveDodgerLeft();
+    }
+
+    
+});
